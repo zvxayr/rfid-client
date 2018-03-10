bfbdbe92f62d8f1f7c7203dbf924aceaace5ad95
zvxayr <carlo4noles@gmail.com>
2018-03-10 22:53:39 +0800

    Added Dotenv

--- client.js
+++ client.js
@@ -4,37 +4,30 @@ const io = require('socket.io-client')
 // replace address with the legit one later
 const socket = io('http://localhost:8080')
 
-/*
-    Features needed:
-    Offine Support
-    Disconnect/Reconnect Logic
-    Data Validation
-*/
+socket.on('accept', (data) => {
+	// Card accepted
+})
 
-// socket.on('connect', function() {});
-// socket.on('event', function(data) {});
-// socket.on('disconnect', function() {});
+socket.on('reject', (data) => {
+	// Card rejected
+})
 
-// void async function() {
-//     let portInfo = (await serialport.list())[0]
+void async function() {
+    let portInfo = (await serialport.list())[0]
 
-//     console.log(portInfo)
+    console.log(portInfo)
 
-//     const parser = new serialport.parsers.Readline('\n')
+    const parser = new serialport.parsers.Readline('\n')
 
-//     const serial = new serialport(portInfo.comName, {
-//         baudRate: 9600
-//     })
+    const serial = new serialport(portInfo.comName, {
+        baudRate: 9600
+    })
 
-//     serial.pipe(parser)
+    serial.pipe(parser)
 
-//     parser.on('data', async data => {
-//         data = data.trim()
+    parser.on('data', async data => {
+        data = data.trim()
 
-//         axios.post('http://localhost:8000', { data })
-//             .then(response => {
-//                 console.log(response.data)
-//             })
-//             .catch(console.error)
-//     })
-// }().catch(console.log)
+        socket.emit('card read', data)
+    })
+}().catch(console.log)
